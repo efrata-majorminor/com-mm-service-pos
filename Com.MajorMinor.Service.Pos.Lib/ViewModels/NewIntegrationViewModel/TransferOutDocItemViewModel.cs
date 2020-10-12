ccd8f18442579cfcd5af0978571e15267c86b991
Andi Budi <andibateeq@gmail.com>
2020-10-12 17:45:01 +0700

    commit

---
+++ Com.MajorMinor.Service.Pos.Lib/ViewModels/NewIntegrationViewModel/TransferOutDocItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.MajorMinor.Service.Pos.Lib.ViewModels.NewIntegrationViewModel
+{
+    public class TransferOutDocItemViewModel
+    {
+        public string articleRealizationOrder { get; set; }
+
+        public ItemViewModel item { get; set; }
+
+        public double quantity { get; set; }
+
+        public string remark { get; set; }
+    }
+}
